dafab831ec742ea77415e30e4f46ced275dde452
shaiprince <42159215+shaiprince@users.noreply.github.com>
2020-11-04 10:20:22 +1100

    Update jk-1CreateMultipleADGroups[InputFromCSV].ps1

--- jk-1CreateMultipleADGroups[InputFromCSV].ps1
+++ jk-1CreateMultipleADGroups[InputFromCSV].ps1
@@ -4,7 +4,6 @@
 	 Created with: 	PowerShell ISE
 	 Created on:   	09/10/2020
 	 Created by:   	Jin
-	 Organization: 	MQ Uni
 	 Filename:     	jk-1CreateMultipleADGroups[InputFromCSV].ps1
 	===========================================================================
 	.DESCRIPTION
