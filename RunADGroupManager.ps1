21623cd6bc8cdec1936815fb501984b8b4677746
shaiprince <42159215+shaiprince@users.noreply.github.com>
2020-12-14 09:45:19 +1100

    Update RunADGroupManager.ps1

--- RunADGroupManager.ps1
+++ RunADGroupManager.ps1
@@ -4,7 +4,6 @@
 	 Created with: 	PowerShell ISE
 	 Created on:   	09/10/2020
 	 Created by:   	Jin
-	 Organization: 	MQ Uni
 	 Filename:     	ADGroupManager.ps1
 	===========================================================================
 	.DESCRIPTION
